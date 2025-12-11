764162d550ee0ab005e878c437228f1f8e764a55
Kamaran Layne <kamaran@layne.dev>
2025-12-10 19:29:12 -0500

    changes to keybd_windows.go: - removed constants for error map. - renamed keyEvent -> newKeyEvent. - removed error maps/slice from funcs. - updated doc comments. - updated typeStr/TypeStr to return single (last) error.

--- keybd_windows.go
+++ keybd_windows.go
@@ -12,13 +12,6 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-// Constants for errors.
-const (
-	E_LOOKUP      = "failed to retrieve scan code"
-	E_SET_MODS    = "failed to set modifier(s)"
-	E_KEY_DOWN_UP = "unexpected error during key press and release"
-)
-
 // Contants for virtual scan codes.
 const (
 	VSC_SPACE      = 0x39
@@ -37,6 +30,7 @@ const (
 	MOD_LALT
 )
 
+// abortFlag is checked in typeStr so that the function can safely abort typing.
 var abortFlag bool
 
 // StandardMods is a [Modifier] slice of the standard modifier keys.
@@ -115,19 +109,19 @@ func KeyIsDown(virtKey byte) bool {
 // [KeyRelease].
 // It returns an error if the call fails.
 func KeyPress(key uint16, flags winapi.KiFlags) error {
-	return winapi.SendInput(keyEvent(key, flags))
+	return winapi.SendInput(newKeyEvent(key, flags))
 }
 
 // KeyRelease sends a key-up event and is intended to be used after a call to
 // [KeyPress].
 // It returns an error if the call fails.
 func KeyRelease(key uint16, flags winapi.KiFlags) error {
-	return winapi.SendInput(keyEvent(key, flags|winapi.KEYEVENTF_KEYUP))
+	return winapi.SendInput(newKeyEvent(key, flags|winapi.KEYEVENTF_KEYUP))
 }
 
 // KeyTap sends a key-down event and a key-up event with a brief pause in
 // between to help simulate an actual keystroke. The duration of the pause is
-// defined by [Global].
+// defined by [KeyPressDuration].
 // It returns an error if the call fails.
 func KeyTap(key uint16, flags winapi.KiFlags) error {
 	var errs []error
@@ -145,11 +139,11 @@ func KeyTap(key uint16, flags winapi.KiFlags) error {
 	return errors.Join(errs...)
 }
 
-// TypeStr types str using the [Global] options and ensures accuracy by
+// TypeStr types str using [TypeString] options and ensures accuracy by
 // attaching the current thread to the thread of the foreground window and
 // temporary blocking input while attached. A timeout prevents the function call
-// from hanging indefinitely, thus allowing the block lift if the timeout is
-// exceeded.
+// from hanging indefinitely while an abort channel allows aborting the
+// operation.
 // It returns an error if the call fails.
 func TypeStr(str string) (err error) {
 	if len(str) == 0 {
@@ -161,52 +155,31 @@ func TypeStr(str string) (err error) {
 	hwnd := windows.GetForegroundWindow()
 
 	var (
-		errs        []error
 		pid         uint32
 		tidAttachTo uint32
 	)
 
 	if hwnd != 0 {
-		tidAttachTo, err = windows.GetWindowThreadProcessId(hwnd, &pid)
-		if err != nil {
-			errs = append(errs, err)
-		}
+		tidAttachTo, _ = windows.GetWindowThreadProcessId(hwnd, &pid)
 	}
 
 	tidAttach := windows.GetCurrentThreadId()
 	attached := false
 	if tidAttachTo != 0 && tidAttach != tidAttachTo {
-		if err = winapi.AttachThreadInput(tidAttach, tidAttachTo, true); err != nil {
-			errs = append(errs, err)
-		} else {
+		if err = winapi.AttachThreadInput(tidAttach, tidAttachTo, true); err == nil {
 			attached = true
-			defer func() {
-				if err = winapi.AttachThreadInput(tidAttach, tidAttachTo, false); err != nil {
-					errs = append(errs, err)
-				}
-			}()
+			defer func() { _ = winapi.AttachThreadInput(tidAttach, tidAttachTo, false) }()
 		}
 	}
-	if err = winapi.BringWindowToTop(hwnd); err != nil {
-		errs = append(errs, err)
-	}
-	if err = winapi.SetForegroundWindow(hwnd); err != nil {
-		errs = append(errs, err)
-	}
-	if _, err = winapi.SetFocus(hwnd); err != nil {
-		errs = append(errs, err)
-	}
+
+	_ = winapi.BringWindowToTop(hwnd)
+	_ = winapi.SetForegroundWindow(hwnd)
+	_, _ = winapi.SetFocus(hwnd)
 
 	blocked := false
-	if err = winapi.BlockInput(true); err != nil {
-		errs = append(errs, err)
-	} else {
+	if err = winapi.BlockInput(true); err == nil {
 		blocked = true
-		defer func() {
-			if err = winapi.BlockInput(false); err != nil {
-				errs = append(errs, err)
-			}
-		}()
+		defer func() { _ = winapi.BlockInput(false) }()
 	}
 
 	TypeString.mu.Lock()
@@ -233,22 +206,24 @@ func TypeStr(str string) (err error) {
 	}
 
 	select {
-	case err := <-done:
-		errs = append(errs, err)
-		return errors.Join(errs...)
+	case typeStrErr := <-done:
+		if typeStrErr != nil {
+			return fmt.Errorf("%s: %v", ErrUncaught, typeStrErr)
+		}
+		return nil
 	case <-ctx.Done():
-		cleanup()
 		abortFlag = true
+		cleanup()
 		return fmt.Errorf("%s", ErrTimeout)
 	case <-abort:
-		cleanup()
 		abortFlag = true
+		cleanup()
 		return fmt.Errorf("%s", ErrAborted)
 	}
 }
 
-// keyEvent creates an input that can be processed by [winapi.SendInput].
-func keyEvent(key uint16, flags winapi.KiFlags) []winapi.INPUT_Ki {
+// newKeyEvent creates an input that can be processed by [winapi.SendInput].
+func newKeyEvent(key uint16, flags winapi.KiFlags) []winapi.INPUT_Ki {
 	ki := winapi.KEYBDINPUT{Vk: 0, Scan: 0, Flags: flags}
 
 	if flags&(winapi.KEYEVENTF_SCANCODE|winapi.KEYEVENTF_UNICODE) != 0 {
@@ -262,47 +237,42 @@ func keyEvent(key uint16, flags winapi.KiFlags) []winapi.INPUT_Ki {
 
 // setMods sets the modifier key state for the current and next iteration of a
 // modifier key.
-func setMods(flags winapi.KiFlags, mods byte, modsNext byte) (modsSet bool, errCount uint) {
+func setMods(flags winapi.KiFlags, mods byte, modsNext byte) bool {
 	var modsSetCount uint
-
 	for _, m := range StandardMods {
 		if mods&m.Mask != 0 {
 			if flags&winapi.KEYEVENTF_KEYUP != 0 {
 				if modsNext&m.Mask == 0 {
-					if err := KeyRelease(m.VSC, flags); err != nil {
-						errCount++
-					}
+					_ = KeyRelease(m.VSC, flags)
+					modsSetCount++
 				}
 			} else {
 				if !KeyIsDown(m.VK) {
-					if err := KeyPress(m.VSC, flags); err != nil {
-						errCount++
-					} else {
-						modsSetCount++
-					}
+					_ = KeyPress(m.VSC, flags)
+					modsSetCount++
 				}
 			}
 		}
 	}
 
-	return modsSetCount > 0, errCount
+	return modsSetCount > 0
 }
 
 // typeStr is the base function for TypeStr that primarily handles the rune
 // translation and the actual key presses.
 func typeStr(str string, hkl winapi.Handle) (err error) {
-	errMap := make(map[string]uint)
 	runes := []rune(str)
 	iLast := len(runes) - 1
 
 	var (
+		errCount int
 		vscNext  uint16
 		modsNext byte
 	)
 
 	vsc, mods, err := RuneToVSC(runes[0], hkl)
 	if err != nil {
-		errMap[E_LOOKUP]++
+		errCount++
 	}
 
 	for i, r := range runes {
@@ -312,9 +282,7 @@ func typeStr(str string, hkl winapi.Handle) (err error) {
 		}
 
 		modFlags := winapi.KEYEVENTF_SCANCODE
-		if modsSet, errCount := setMods(modFlags, mods, 0); errCount != 0 {
-			errMap[E_SET_MODS] += errCount
-		} else if modsSet {
+		if modsSet := setMods(modFlags, mods, 0); modsSet {
 			time.Sleep(TypeString.ModPressDuration)
 		}
 
@@ -326,23 +294,22 @@ func typeStr(str string, hkl winapi.Handle) (err error) {
 
 		for range numTaps {
 			if err = KeyTap(vsc, modFlags); err != nil {
-				errMap[E_KEY_DOWN_UP]++
+				errCount++
 			}
 		}
-
-		modFlags |= winapi.KEYEVENTF_KEYUP
 
 		if i < iLast {
 			vscNext, modsNext, err = RuneToVSC(runes[i+1], hkl)
 			if err != nil {
-				errMap[E_LOOKUP]++
+				errCount++
 			}
 		} else if i == iLast {
 			modsNext = 0
 		}
-		if _, errCount := setMods(modFlags, mods, modsNext); errCount != 0 {
-			errMap[E_SET_MODS] += errCount
-		}
+
+		modFlags |= winapi.KEYEVENTF_KEYUP
+		_ = setMods(modFlags, mods, modsNext)
+
 		if i < iLast {
 			vsc = vscNext
 			mods = modsNext
@@ -350,12 +317,9 @@ func typeStr(str string, hkl winapi.Handle) (err error) {
 		}
 	}
 
-	var errs []error
-	if len(errMap) > 0 {
-		for msg, msgCount := range errMap {
-			errs = append(errs, fmt.Errorf("(x %d) %s", msgCount, msg))
-		}
+	if errCount == 0 {
+		return nil
 	}
 
-	return errors.Join(errs...)
+	return err
 }
